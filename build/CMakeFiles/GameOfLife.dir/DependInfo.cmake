360fb7de0efc4630930e3122882fb2033408fb8a
pdenning <pdenning1993@gmail.com>
2022-11-12 16:13:44 +0000

    Changes to CMakeLists.txt

---
+++ build/CMakeFiles/GameOfLife.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pdenn/Code/capstone/src/Board.cpp" "/home/pdenn/Code/capstone/build/CMakeFiles/GameOfLife.dir/src/Board.cpp.o"
+  "/home/pdenn/Code/capstone/src/Button.cpp" "/home/pdenn/Code/capstone/build/CMakeFiles/GameOfLife.dir/src/Button.cpp.o"
+  "/home/pdenn/Code/capstone/src/GameManager.cpp" "/home/pdenn/Code/capstone/build/CMakeFiles/GameOfLife.dir/src/GameManager.cpp.o"
+  "/home/pdenn/Code/capstone/src/SDLManager.cpp" "/home/pdenn/Code/capstone/build/CMakeFiles/GameOfLife.dir/src/SDLManager.cpp.o"
+  "/home/pdenn/Code/capstone/src/main.cpp" "/home/pdenn/Code/capstone/build/CMakeFiles/GameOfLife.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GameOfLife.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameOfLife.dir/src/Board.cpp.o"
+  "CMakeFiles/GameOfLife.dir/src/Button.cpp.o"
+  "CMakeFiles/GameOfLife.dir/src/GameManager.cpp.o"
+  "CMakeFiles/GameOfLife.dir/src/SDLManager.cpp.o"
+  "CMakeFiles/GameOfLife.dir/src/main.cpp.o"
+  "GameOfLife"
+  "GameOfLife.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameOfLife.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
