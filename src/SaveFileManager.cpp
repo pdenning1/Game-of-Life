b1e7a380d9f6d870ad93e4488a504c5d37f161cc
pdenning <pdenning1993@gmail.com>
2022-11-12 18:28:49 +0000

    still working on SaveFileManager

--- src/SaveFileManager.cpp
+++ src/SaveFileManager.cpp
@@ -4,13 +4,20 @@
 std::string SaveFileManager::loadBoardLayout()
 {
     std::string json = "";
+    std::string line;
 
     std::ifstream filestream(saveFileName);
     if (filestream.is_open())
     {
-        while (std::getline(filestream, json))
+        while (std::getline(filestream, line))
         {
+            json += line;
             std::cout << json << std::endl;
+        }
+
+        if (json.compare("") != 0)
+        {
+            // parse json here
         }
     }
 
